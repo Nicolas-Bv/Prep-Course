7820dd0a967357b1013973827812819c299458b5
Nicolas Morgan <nicolas.bv@outlook.com>
2022-03-21 23:07:07 -0300

    Agregado nuevo archivo, archivo-ejemplo.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -255,7 +255,7 @@ function esVocal(letra){
   //Escribe tu código aquí
   if (letra==="a"||letra==="e"||letra==="i"||letra==="o"||letra==="u"){
     return "Es vocal";
-  } else if (letra.lenght!==1){
+  } else if (letra.length!==1){
     return "Dato incorrecto";
   } else {
     return "Dato incorrecto"
